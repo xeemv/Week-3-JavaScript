87f12714c37e9531cc4a76ade9a8e7d1abc7951d
xeemv <xeeyang08@gmail.com>
2021-12-12 23:41:51 -0600

    update array'

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -4,3 +4,11 @@ c // position 2
 d // position 3
 e // position 4
 f // position 5 = the length of the element (6) - 1 = position 5
+//
+
+//creating a customer's name array
+
+var customerNames = [];
+customerNames.push['Sam Smith']; //put the customer's name in the square brackets and use quotes
+// dont forget that there is zero based indexing. 'Sam Smith' = array position zero
+customerNames.push['Tommy Guns']; //array position 1
