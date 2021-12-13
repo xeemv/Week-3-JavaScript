6100f3ee58fc49ed979e11f9001d5d8acd7cf5f4
xeemv <xeeyang08@gmail.com>
2021-12-13 00:23:08 -0600

    changes to customerNames array using for loop

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -9,8 +9,14 @@
 //creating a customer's name array
 
 var customerNames = [];
-customerNames.push('Sam Smith'); //put the customer's name in the square brackets and use quotes
+customerNames.push('Sam Smith'); //put the customer's name in the parentheses and use quotes
 // dont forget that there is zero based indexing. 'Sam Smith' = array position zero
 customerNames.push('Tommy Guns'); //array position 1
 
-console.log(customerNames[0]); //testing to see if we are able to open array position zero on the browser
+for (i = 0; i < customerNames.length; i++) {
+// console.log(customerNames[1]); //testing to see if we are able to open array position zero on the browser
+console.log(customerNames[i]); //using i in replacement of arrary position 0 or 1
+}
+
+// we can use a loop here to iterate over customer names and print each name out so instead of having two lines to print each one out ===> using the customerNames array function
+// creating a new for loop on line 16
