bfdeb3fb4a43615c25ed899b4a62e951b96835b5
xeemv <xeeyang08@gmail.com>
2021-12-13 00:01:41 -0600

    added customerNames array

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -1,14 +1,16 @@
-a // position zero = in an array
-b // position 1
-c // position 2
-d // position 3
-e // position 4
-f // position 5 = the length of the element (6) - 1 = position 5
+// a // position zero = in an array
+// b // position 1
+// c // position 2
+// d // position 3
+// e // position 4
+// f // position 5 = the length of the element (6) - 1 = position 5
 //
 
 //creating a customer's name array
 
 var customerNames = [];
-customerNames.push['Sam Smith']; //put the customer's name in the square brackets and use quotes
+customerNames.push('Sam Smith'); //put the customer's name in the square brackets and use quotes
 // dont forget that there is zero based indexing. 'Sam Smith' = array position zero
-customerNames.push['Tommy Guns']; //array position 1
+customerNames.push('Tommy Guns'); //array position 1
+
+console.log(customerNames[0]); //testing to see if we are able to open array position zero on the browser
